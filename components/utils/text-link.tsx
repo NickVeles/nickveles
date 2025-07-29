6445d7089e81e8eaa63d3a19a0c6dcdec22a771d
NickVeles <xm1k123x@gmail.com>
2025-07-30 01:29:02 +0200

    feat: extend text link by adjustable classname

--- components/utils/text-link.tsx
+++ components/utils/text-link.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import { IconExternalLink } from "@tabler/icons-react";
+import { cn } from "@/lib/utils";
 
 type TextLinkProps = {
   href: string;
   isIcon?: boolean;
   target?: string;
+  className?: string;
   children: React.ReactNode;
 };
 
@@ -12,11 +14,12 @@ export default function TextLink({
   href,
   isIcon,
   target,
+  className,
   children,
 }: TextLinkProps) {
   return (
     <Link
-      className="text-link visited:text-accent hover:underline"
+      className={cn("text-link visited:text-accent hover:underline", className)}
       href={href}
       target={target}
       rel={target === "_blank" ? "noopener noreferrer" : undefined}
