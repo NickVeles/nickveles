a45d85c5648a64d5060dbfb8790b5dd224a10e89
NickVeles <xm1k123x@gmail.com>
2025-07-30 19:30:42 +0200

    feat: add pc menu

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,14 @@
 import MobileMenu from "./utils/mobile-menu";
+import PCMenu from "./utils/pc-menu";
 
 export default function Header() {
-
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 font-sans">
-      <div className="container flex h-16 items-center px-4">
-        {/* Mobile Menu - Visible on mobile, hidden on sm+ */}
-        <MobileMenu className="sm:hidden" />
-
-        {/* Logo - Visible on sm+ */}
+      <div className="flex h-16 items-center px-4">
+        <div className="flex flex-1">
+          <MobileMenu className="sm:hidden" />
+          <PCMenu className="hidden sm:flex" />
+        </div>
       </div>
     </header>
   );
---
+++ components/utils/pc-menu.tsx
@@ -0,0 +1,91 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuContent,
+  NavigationMenuTrigger,
+} from "../ui/navigation-menu";
+import { sitemap } from "@/constants/sitemap";
+
+// Custom data
+import LogomarkSVG from "@/assets/icons/logomark.svg";
+import { ExternalLinkIcon } from "lucide-react";
+import SocialNavigation from "./social-navigation";
+
+type PCMenuProps = {
+  className?: string;
+};
+
+export default function PCMenu({ className }: PCMenuProps) {
+  return (
+    <div className={cn("flex justify-between items-center w-full", className)}>
+      {/* Left side */}
+      <NavigationMenu viewport={false}>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/">
+              <Image
+                src={LogomarkSVG}
+                alt="Home"
+                width={24}
+                height={24}
+                className="dark:invert"
+              />
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          {/* Rest of navigation */}
+          {sitemap.navigation.slice(1).map(({ name, url }) => (
+            <NavigationMenuItem key={name}>
+              <NavigationMenuLink
+                href={url}
+                className={
+                  name === "Hire/Contact"
+                    ? "text-secondary-foreground rounded-sm hover:bg-primary hover:text-primary-foreground"
+                    : ""
+                }
+              >
+                {name}
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
+
+          {/* Affiliates */}
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Affiliates</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="min-w-[250px] gap-1">
+                {sitemap.affiliates.map(({ name, url, image }) => (
+                  <li key={name} className="w-full">
+                    <NavigationMenuLink
+                      href={url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-flex flex-row items-center w-full"
+                    >
+                      <Image
+                        src={image}
+                        alt={name}
+                        width={24}
+                        height={24}
+                        className="dark:invert mr-1"
+                      />
+                      {name}
+                      <ExternalLinkIcon className="ml-auto size-4 text-foreground" />
+                    </NavigationMenuLink>
+                  </li>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      {/* Right Side */}
+      <SocialNavigation />
+    </div>
+  );
+}
