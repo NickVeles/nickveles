cd1d942e4988e416a47cce7b85d4f82792d5200e
NickVeles <xm1k123x@gmail.com>
2025-07-30 21:32:23 +0200

    feat: improve loading component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  const [dots, setDots] = useState("");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length >= 3 ? "" : prev + "."));
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      className="flex flex-col items-center justify-center gap-4 font-sans"
+      style={{ height: "calc(100vh - 88px)" }}
+    >
+      <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin" />
+      Loading{dots}
+    </div>
+  );
 }
