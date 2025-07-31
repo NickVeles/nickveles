746b07f68ba9c2b89cb3d529d73ee537fd18ae1a
NickVeles <xm1k123x@gmail.com>
2025-07-31 19:07:56 +0200

    fix: add temporary lorem and remove import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Main from "@/components/main";
-
 export default function Home() {
   return (
-    <p>hello</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis qui velit iusto temporibus fugiat ut sequi aperiam provident repudiandae vitae beatae non aliquid quidem distinctio, nesciunt eveniet. Odio, recusandae sed?</p>
   );
 }
