858bc58538e7280d26d99c991b73153c1b3a5985
NickVeles <xm1k123x@gmail.com>
2025-07-31 20:28:09 +0200

    feat: remove horizontal padding from main

--- components/main.tsx
+++ components/main.tsx
@@ -7,7 +7,7 @@ type MainProps = {
 export default function Main({ children }: MainProps) {
   return (
     <main
-      className="flex flex-col flex-1 min-h-(--main-min-height) justify-center items-center px-4 sm:px-24 lg:px-32 py-8 sm:py-12 gap-8 font-sans"
+      className="flex flex-col flex-1 min-h-(--main-min-height) justify-center items-center py-8 sm:py-12 gap-8 font-sans"
     >
       {children}
     </main>
