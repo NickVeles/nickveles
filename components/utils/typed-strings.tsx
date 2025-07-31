737c58b5c1d0b6a1171dd4dde1a5c6eed1c01a1e
NickVeles <xm1k123x@gmail.com>
2025-07-31 20:48:53 +0200

    feat: add typedstrings animation

---
+++ components/utils/typed-strings.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
+type TypedStringsProps = {
+  list: string[];
+} & React.HTMLAttributes<HTMLParagraphElement>;
+
+export default function TypedStrings({ list, ...props }: TypedStringsProps) {
+  const [currentStringIndex, setCurrentStringIndex] = useState(0);
+  const [displayedText, setDisplayedText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  // Typing animation effect
+  useEffect(() => {
+    const currentString = list[currentStringIndex];
+    const typingSpeed = isDeleting ? 50 : 100;
+    const pauseTime = isDeleting ? 500 : 2000;
+
+    const timer = setTimeout(() => {
+      if (!isDeleting && displayedText === currentString) {
+        setTimeout(() => setIsDeleting(true), pauseTime);
+      } else if (isDeleting && displayedText === "") {
+        setIsDeleting(false);
+        setCurrentStringIndex((prev) => (prev + 1) % list.length);
+      } else {
+        setDisplayedText((prev) =>
+          isDeleting
+            ? prev.slice(0, -1)
+            : currentString.slice(0, prev.length + 1)
+        );
+      }
+    }, typingSpeed);
+
+    return () => clearTimeout(timer);
+  }, [displayedText, isDeleting, currentStringIndex]);
+  return (
+    <p {...props}>
+      {displayedText}
+      <span
+        className={cn(
+          "text-primary-highlighter",
+          !isDeleting && displayedText === list[currentStringIndex]
+            ? "animate-blink"
+            : ""
+        )}
+      >
+        |
+      </span>
+    </p>
+  );
+}
