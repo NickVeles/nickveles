38ae50896e0e50a6cc478f71bb398f0aa2316e9d
NickVeles <xm1k123x@gmail.com>
2025-08-01 23:36:33 +0200

    fix: add missing type prop

--- types/yt-video.ts
+++ types/yt-video.ts
@@ -1,5 +1,8 @@
+import YouTubeVideoCategory from "./yt-video-category";
+
 export default interface YouTubeVideo {
   _id: string;
   title: string;
   youtubeUrl: string;
+  category: YouTubeVideoCategory;
 }
