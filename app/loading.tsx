76f0595898993756220f61a6813f92d4ee952310
NickVeles <xm1k123x@gmail.com>
2025-08-01 15:15:59 +0200

    feat: make loading text conditional

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,14 @@
 import LoadingText from "@/components/utils/loading-text";
 
-export default function Loading() {
+type LoadingProps = {
+  showLoading?: boolean;
+};
+
+export default function Loading({ showLoading }: LoadingProps) {
   return (
-    <div
-      className="flex flex-col items-center justify-center gap-4 font-sans"
-    >
+    <div className="flex flex-col items-center justify-center gap-4 font-sans">
       <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin" />
-      <LoadingText />
+      {showLoading && <LoadingText />}
     </div>
   );
 }
