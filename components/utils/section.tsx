dbb62829f6358b042a6ca351dddeeaff932f7383
NickVeles <xm1k123x@gmail.com>
2025-08-02 17:14:44 +0200

    feat: use container styling in section

--- components/utils/section.tsx
+++ components/utils/section.tsx
@@ -16,7 +16,7 @@ export default function Section({
     <section
       id={id}
       className={cn(
-        "flex flex-col justify-center items-center w-full p-4 sm:py-4 sm:px-[clamp(1rem,5vw,8rem)] gap-12",
+        "flex flex-col justify-center items-center container gap-12",
         className
       )}
       {...props}
