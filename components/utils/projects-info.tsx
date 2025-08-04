38d8736b11c2dadacb762c9edbfe9243f4db25b1
NickVeles <xm1k123x@gmail.com>
2025-08-04 20:25:42 +0200

    feat: improve styling

--- components/utils/projects-info.tsx
+++ components/utils/projects-info.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import SectionText from "@/types/section-text";
+import { SectionH } from "../ui/typography";
 
 type ProjectsInfoProps = {
   textObject: SectionText;
@@ -10,18 +11,20 @@ export default function ProjectsInfo({ textObject }: ProjectsInfoProps) {
   return (
     <div className="flex flex-col flex-1 gap-4 max-w-xl">
       <div className="flex flex-col gap-12">
-        <h2 className="font-bold tracking-tighter text-5xl md:text-6xl">
+        <SectionH className="md:text-start">
           {textObject.title ?? "Projects"}
-        </h2>
+        </SectionH>
         <div className="flex flex-col text-justify gap-4 text-xl w-full indent-4 dyslexic:font-dyslexic dyslexic:text-lg text-muted-foreground">
           {textObject.paragraphs?.map((paragraph, index) => (
             <p key={index}>{paragraph}</p>
           ))}
         </div>
       </div>
-      <Button size="lg" className="w-fit mt-4" asChild>
-        <Link href="/portfolio">See All Projects</Link>
-      </Button>
+      <div className="w-full flex justify-center md:justify-start">
+        <Button size="lg" className="w-fit mt-4" asChild>
+          <Link href="/portfolio">See All Projects</Link>
+        </Button>
+      </div>
     </div>
   );
 }
