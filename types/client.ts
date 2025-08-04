ead04d051a79afcdbd07b63e6089702f546ab490
NickVeles <xm1k123x@gmail.com>
2025-08-04 17:34:35 +0200

    feat: join referral and client into one type

--- types/client.ts
+++
@@ -1,6 +0,0 @@
-export default interface Client {
-  _id: string;
-  name: string;
-  logo: any;
-  website?: string;
-}
---
+++ types/referrer.ts
@@ -0,0 +1,8 @@
+export default interface Referrer {
+  _id: string;
+  name: string;
+  logo?: any;
+  website?: string;
+  referral?: string;
+  score?: number;
+}
