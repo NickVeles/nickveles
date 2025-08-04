654679082592dd1b01a8b29a680bed8bd230d1bb
NickVeles <xm1k123x@gmail.com>
2025-08-04 16:59:50 +0200

    fix: minor appearing change

--- components/utils/appearing-div.tsx
+++ components/utils/appearing-div.tsx
@@ -8,7 +8,7 @@ export const AppearingDiv = ({ children }: { children: React.ReactNode }) => {
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6, ease: 'easeOut' }}
-      viewport={{ once: true, amount: 0.1 }}
+      viewport={{ once: true, amount: 0.15 }}
       className="w-full"
     >
       {children}
