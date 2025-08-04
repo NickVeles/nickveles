088f7b2012d5d4c25d5d47c88f9f1254dee5c039
NickVeles <xm1k123x@gmail.com>
2025-08-04 23:12:05 +0200

    feat: convert Client type into class for custom init

--- types/client.ts
+++ types/client.ts
@@ -1,7 +1,26 @@
-export default interface Client {
+import { urlFor } from "@/lib/sanity-image";
+
+export default class Client {
   _id: string;
   name: string;
   personTitle?: string;
   logo?: any;
+  resolvedLogo?: string;
   website?: string;
+
+  constructor(data: {
+    _id: string;
+    name: string;
+    personTitle?: string;
+    logo?: any;
+    website?: string;
+  }) {
+    this._id = data._id;
+    this.name = data.name;
+    this.personTitle = data.personTitle;
+    this.logo = data.logo;
+    this.website = data.website;
+
+    this.resolvedLogo = this.logo ? urlFor(this.logo).width(64).height(64).url() : undefined;
+  }
 }
