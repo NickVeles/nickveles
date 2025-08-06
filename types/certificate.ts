42ba8e22c876e2181549879fce893f163fe9429a
NickVeles <xm1k123x@gmail.com>
2025-08-06 23:46:25 +0200

    fix: revert class to interface

--- types/certificate.ts
+++ types/certificate.ts
@@ -1,33 +1,9 @@
-export default class Certificate {
+export default interface Certificate {
   _id: string;
   title: string;
   description?: string;
   issuer: string;
   date: string;
   url?: string;
-  file?: any;
-
-  constructor(data: {
-    _id: string;
-    title: string;
-    description?: string;
-    issuer: string;
-    date: string;
-    url?: string;
-    file?: any;
-  }) {
-    this._id = data._id;
-    this.title = data.title;
-    this.description = data.description;
-    this.issuer = data.issuer;
-    this.date = data.date;
-    this.file = data.file;
-
-    // Overwrite url from file.asset.url if available
-    if (data.file?.asset?.url) {
-      this.url = data.file.asset.url;
-    } else {
-      this.url = data.url;
-    }
-  }
+  file?: { asset: { url: string }};
 }
