1c9ab5eea6daab3623fafe07f9e89363f704fe87
NickVeles <xm1k123x@gmail.com>
2025-08-06 23:39:41 +0200

    fix: change prop name for clarity

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,14 +1,14 @@
 import LoadingText from "@/components/utils/loading-text";
 
 type LoadingProps = {
-  showLoading?: boolean;
+  showLoadingText?: boolean;
 };
 
-export default function Loading({ showLoading }: LoadingProps) {
+export default function Loading({ showLoadingText }: LoadingProps) {
   return (
     <div className="flex flex-col items-center justify-center gap-4 font-sans">
       <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin" />
-      {showLoading && <LoadingText />}
+      {showLoadingText && <LoadingText />}
     </div>
   );
 }
