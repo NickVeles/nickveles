511844f34d38d7daaebd7b9311e7e3e62db78d7b
NickVeles <xm1k123x@gmail.com>
2025-08-06 19:43:20 +0200

    feat: turn certificate into a class with custom constructor to handle file urls

--- types/certificate.ts
+++ types/certificate.ts
@@ -1,4 +1,4 @@
-export default interface Certificate {
+export default class Certificate {
   _id: string;
   title: string;
   description?: string;
@@ -6,4 +6,28 @@ export default interface Certificate {
   date: string;
   url?: string;
   file?: any;
+
+  constructor(data: {
+    _id: string;
+    title: string;
+    description?: string;
+    issuer: string;
+    date: string;
+    url?: string;
+    file?: any;
+  }) {
+    this._id = data._id;
+    this.title = data.title;
+    this.description = data.description;
+    this.issuer = data.issuer;
+    this.date = data.date;
+    this.file = data.file;
+
+    // Overwrite url from file.asset.url if available
+    if (data.file?.asset?.url) {
+      this.url = data.file.asset.url;
+    } else {
+      this.url = data.url;
+    }
+  }
 }
