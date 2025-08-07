2eb2680fd3577e3908d4dc482124802af7bcb6f3
NickVeles <xm1k123x@gmail.com>
2025-08-07 15:34:56 +0200

    feat: add disableAnims prop to section and refine section styling

--- components/utils/section.tsx
+++ components/utils/section.tsx
@@ -1,24 +1,29 @@
+import { cn } from "@/lib/utils";
 import { AppearingDiv } from "./appearing-div";
 
 type SectionProps = React.HTMLAttributes<HTMLElement> & {
   id: string;
+  disableAnims?: boolean;
   className?: string;
   children?: React.ReactNode;
 };
 
 export default function Section({
   id,
+  disableAnims,
   className,
   children,
   ...props
 }: SectionProps) {
+  const refinedClassName = cn("w-full flex flex-col px-4", className);
+
   return (
-    <section
-      id={id}
-      className="container last:mb-32"
-      {...props}
-    >
-      <AppearingDiv className={className}>{children}</AppearingDiv>
+    <section id={id} className="container last:mb-32" {...props}>
+      {disableAnims ? (
+        <div className={refinedClassName}>{children}</div>
+      ) : (
+        <AppearingDiv className={refinedClassName}>{children}</AppearingDiv>
+      )}
     </section>
   );
 }
