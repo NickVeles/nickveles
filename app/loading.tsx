df72ca8a403e5fd044bbd868f26de788df665ed8
NickVeles <xm1k123x@gmail.com>
2025-08-09 13:15:40 +0200

    feat: add className to loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,15 @@
 import LoadingText from "@/components/utils/loading-text";
+import { cn } from "@/lib/utils";
 
 type LoadingProps = {
   showLoadingText?: boolean;
+  className?: string;
 };
 
-export default function Loading({ showLoadingText }: LoadingProps) {
+export default function Loading({ showLoadingText, className }: LoadingProps) {
   return (
     <div className="flex flex-col items-center justify-center gap-4 font-sans">
-      <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin" />
+      <div className={cn("w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin", className)} />
       {showLoadingText && <LoadingText />}
     </div>
   );
