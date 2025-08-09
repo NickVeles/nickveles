838a5ced3a26dabd36a939d587aa18500217b37a
NickVeles <xm1k123x@gmail.com>
2025-08-09 17:34:46 +0200

    feat: add variants to disclaimer

--- components/disclaimer.tsx
+++ components/disclaimer.tsx
@@ -1,23 +1,42 @@
 "use client";
 
 import { ReactNode, useState } from "react";
-import { CircleAlertIcon, XIcon } from "lucide-react";
+import { CircleAlertIcon, InfoIcon, XIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 type DisclaimerProps = {
   children?: ReactNode;
+  variant?: "default" | "primary" | "destructive";
 };
 
-export default function Disclaimer({ children }: DisclaimerProps) {
+export default function Disclaimer({
+  children,
+  variant = "default",
+}: DisclaimerProps) {
   const [visible, setVisible] = useState(true);
+  const iconStyle = "size-4 min-w-4 min-h-4";
+  const discStyle =
+    variant === "destructive"
+      ? "bg-destructive text-destructive-foreground"
+      : "bg-primary text-primary-foreground";
 
   if (!visible || !children) return null;
 
   return (
-    <div className="w-full bg-destructive border-b text-destructive-foreground flex justify-center items-center">
+    <div
+      className={cn(
+        "w-full border-b flex justify-center items-center",
+        discStyle
+      )}
+    >
       <div className="px-4 py-1 w-full flex justify-between items-center">
         <div className="flex items-center gap-1">
-          <CircleAlertIcon className="size-4 min-w-4 min-h-4" />
+          {variant === "destructive" ? (
+            <CircleAlertIcon className={iconStyle} />
+          ) : (
+            <InfoIcon className={iconStyle} />
+          )}
           <p className="text-xs font-medium inline">{children}</p>
         </div>
         <Button
