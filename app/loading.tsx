69bd97786cfd03ac99fc3afd6bb9b7a14ed9b4cc
NickVeles <xm1k123x@gmail.com>
2025-08-09 13:50:54 +0200

    feat: replace loading div with an icon

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,6 @@
 import LoadingText from "@/components/utils/loading-text";
 import { cn } from "@/lib/utils";
+import { LoaderCircleIcon } from "lucide-react";
 
 type LoadingProps = {
   showLoadingText?: boolean;
@@ -14,12 +15,7 @@ export default function Loading({
 }: LoadingProps) {
   return (
     <div className="flex flex-col items-center justify-center gap-4 font-sans">
-      <div
-        className={cn(
-          "size-12 border-4 border-primary border-t-transparent rounded-full animate-spin",
-          className
-        )}
-      />
+      <LoaderCircleIcon className={cn("size-12 animate-spin", className)} />
       {showLoadingText && <LoadingText>{loadingText}</LoadingText>}
     </div>
   );
