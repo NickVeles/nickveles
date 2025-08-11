ec6542ef8b08b7ad511aa731653bdc98f74266d1
NickVeles <xm1k123x@gmail.com>
2025-08-11 21:23:43 +0200

    feat: create portfolio page (wip)

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import Section from "@/components/utils/section";
+
+export default async function Portfolio() {
+  // Fetch data from Sanity
+
+  return (
+    <Section id="portfolio">
+
+    </Section>
+  )
+}
