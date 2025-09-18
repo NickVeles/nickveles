d2bfad7e34d4b7e2b37ba4f6a918560a3f622f4d
NickVeles <xm1k123x@gmail.com>
2025-09-18 18:26:53 +0200

    feat: add metadata to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,21 @@
-import PasswordProtected from '@/components/profile/password-protected'
+import PasswordProtected from "@/components/profile/password-protected";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://nickveles.com/"),
+  title: "Profile",
+  description:
+    "A profile page with personal information about me, intended for friends and trusted individuals who want to get to know me beyond my professional life.",
+  openGraph: {
+    title: "Profile | Nick Veles",
+    description:
+      "A profile page with personal information about me, intended for friends and trusted individuals who want to get to know me beyond my professional life.",
+    images: ["/og-image.jpg"],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function ProtectedPage() {
-  return (
-    <PasswordProtected>
-      test
-    </PasswordProtected>
-  )
+  return <PasswordProtected>test</PasswordProtected>;
 }
