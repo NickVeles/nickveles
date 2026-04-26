724b25a843d00a7e4c35fd11fe863551f4029255
NickVeles <xm1k123x@gmail.com>
2026-04-26 20:14:24 +0200

    refactor: simplify Skills component header structure

--- components/home/skills.tsx
+++ components/home/skills.tsx
@@ -7,7 +7,9 @@ import { SkillCategory } from "@/types/skill-category";
 
 export default async function Skills() {
   // Get individual skills
-  const skills = await getSanityData<Skill[]>(`*[_type == "skill" && category->level >= 0]{
+  const skills = await getSanityData<
+    Skill[]
+  >(`*[_type == "skill" && category->level >= 0]{
     _id,
     name,
     category->{
@@ -35,12 +37,7 @@ export default async function Skills() {
 
   return (
     <Section id="skills" className="gap-6">
-      <SectionH>
-        <span className="text-lg tracking-wide">
-          The <span className="font-bold text-xl">WALL</span> of{" "}
-        </span>
-        Skills
-      </SectionH>
+      <SectionH>Skills</SectionH>
       {isRenderSkills && (
         <SkillSearch items={skills} categories={skillCategories} />
       )}
